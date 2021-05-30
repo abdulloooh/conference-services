cddf42838cac263880798b791f77fb827d7df1dc
abdulloooh <abdullahakinwumi@gmail.com>
2021-05-30 13:26:46 +0100

    register service to registry & bare routes

--- speakers-service/server/service.js
+++ speakers-service/server/service.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const service = express();
 
 module.exports = (config) => {
   const log = config.log();
   // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
+  if (service.get("env") === "development") {
     service.use((req, res, next) => {
       log.debug(`${req.method}: ${req.url}`);
       return next();
     });
   }
+
+  service.get("/list", (req, res, next) => {
+    return next("Not Implemented");
+  });
+
+  service.get("/list-short", (req, res, next) => {
+    return next("Not Implemented");
+  });
+
+  service.get("/names", (req, res, next) => {
+    return next("Not Implemented");
+  });
+
+  service.get("/artwork/:shortname", (req, res, next) => {
+    return next("Not Implemented");
+  });
+
+  service.get("/speaker/:shortname", (req, res, next) => {
+    return next("Not Implemented");
+  });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
