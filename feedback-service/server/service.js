67264c2a1d25896f26c03512f2dfe17a0a42160d
abdulloooh <abdullahakinwumi@gmail.com>
2021-06-03 03:40:33 +0100

    setting up feedback service

---
+++ feedback-service/server/service.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const service = express();
+
+const Feedback = require("./lib/Feedback");
+
+module.exports = (config) => {
+  const log = config.log();
+
+  const feedback = new Feedback(config.data.feedback);
+
+  // Add a request logging middleware in development mode
+  if (service.get("env") === "development") {
+    service.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return service;
+};
