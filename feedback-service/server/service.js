ca0f71eef2d5523a15b64153365746cfbbad09ca
abdulloooh <abdullahakinwumi@gmail.com>
2021-06-08 19:55:22 +0100

    consume from rabbitmq & tidy up

--- feedback-service/server/service.js
+++ feedback-service/server/service.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const express = require("express");
 const service = express();
+const amqplib = require("amqplib")
 
 const bodyParser = require("body-parser");
 
@@ -14,6 +15,19 @@ module.exports = (config) => {
   service.use(bodyParser.json());
   service.use(bodyParser.urlencoded({ extended: true }));
 
+  const q = "feedback"
+
+  amqplib.connect('amqp://localhost').then(conn => conn.createChannel())
+    .then(ch => ch.assertQueue(q)
+      .then(()=>ch.consume(q, (msg) =>{
+        if(msg !== null){
+          log.debug (`Got message ${msg.content.toString()}`);
+          const qm = JSON.parse(msg.content.toString());
+          feedback.addEntry(qm.name,qm.title,qm.message)
+            .then(()=>ch.ack(msg))
+        }
+    }))).catch(err => log.fatal(err))
+
   // Add a request logging middleware in development mode
   if (service.get("env") === "development") {
     service.use((req, res, next) => {
@@ -24,11 +38,11 @@ module.exports = (config) => {
 
   service.get("/list", async (req, res) => res.json(await feedback.getList()));
 
-  service.post("/add-entry", async (req, res) => {
-    const { name, title, message } = req.body;
-    await feedback.addEntry(name, title, message);
-    return res.sendStatus(200);
-  });
+  // service.post("/add-entry", async (req, res) => {
+  //   const { name, title, message } = req.body;
+  //   await feedback.addEntry(name, title, message);
+  //   return res.sendStatus(200);
+  // });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
